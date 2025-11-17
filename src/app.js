4ee5145bfd09a2b9973ba930172b655567d9c4f7
gkapo23 <gilbert.afari@cirvianum.cat>
2025-11-17 18:57:16 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const prono = ["el", "la", "los", "tu"];
+const nom = ["vecino", "familiar", "padre", "serie"];
+const adj = ["favorita", "alto", "guapo", "bajito"];
+const ext = [".com", ".es", ".org", ".wiki"];
+
+window.onload = function () {
+  for (let p of prono) {
+    for (let n of nom) {
+      for (let a of adj) {
+        for (let e of ext) {
+          console.log(p + n + a + e);
+        }
+      }
+    }
+  }
 };
